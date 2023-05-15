0780977e5cb390f571cf0015c004c001b9667e02
RyanHalliwellAI <ryan.halliwellai@gmail.com>
2023-05-16 08:49:30 +0930

    Adding to Model

---
+++ src/main/java/com/banksystem/model/Account.java
@@ -0,0 +1,9 @@
+package com.banksystem.model;
+
+public class Account {
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    // Will add Constructor, getters, and setters
+}

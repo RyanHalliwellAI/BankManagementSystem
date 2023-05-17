64890991cfb68fcfff62bfaa801124a765697621
RyanHalliwellAI <ryan.halliwellai@gmail.com>
2023-05-17 10:22:37 +0930

    Adding to View and Controller

---
+++ src/main/java/com/banksystem/controller/BankController.java
@@ -0,0 +1,15 @@
+package com.banksystem.controller;
+
+import com.banksystem.model.Account;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankController {
+    private List<Account> accounts;
+
+    public BankController() {
+        this.accounts = new ArrayList<>();
+    }
+
+    //Adding accounts in future
+}
